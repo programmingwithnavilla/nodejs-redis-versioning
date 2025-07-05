00a7a9d01cedc0d7b730c8976285bdba1b9e2300
programmingwithnavilla <navidbarsalari@gmail.com>
2025-07-05 21:32:58 +0330

    change redis config

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -2,9 +2,6 @@ import dotenv from 'dotenv';
 import { Redis } from 'ioredis';
 
 dotenv.config();
-const redis = new Redis({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: Number(process.env.REDIS_PORT) || 6379,
-});
+const redis = new Redis(process.env.REDIS_URL!);
 
 export default redis;
