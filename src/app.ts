25f69c8eb26f4b7ad56f7ff06c4d6902e53ccc70
programmingwithnavilla <navidbarsalari@gmail.com>
2025-07-05 21:54:41 +0330

    fix ts config

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import userRoutes from './routes/userRoutes';
+import { default as userRouter } from './modules/users/user.route';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.use('/users', userRoutes);
+app.use('/users', userRouter);
 
 export default app;
--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -7,6 +7,7 @@ const router = Router();
 const userController = container.resolve(UserController);
 
 router.get('/', userController.getAll);
+// router.get('/:id', userController.getById);
 router.post('/', userController.create);
 router.put('/:id', userController.update);
 router.delete('/:id', userController.delete);
