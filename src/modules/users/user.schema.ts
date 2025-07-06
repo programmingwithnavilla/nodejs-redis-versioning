0816fa61e7c0756c11e46dac8f9e7de56c710ccb
programmingwithnavilla <navidbarsalari@gmail.com>
2025-07-06 18:04:11 +0330

    add param in user

--- src/modules/users/user.schema.ts
+++ src/modules/users/user.schema.ts
@@ -12,6 +12,10 @@ export const UpdateUserSchema = z.object({
   name: z.string().min(2).max(50).optional(),
   email: z.string().email().optional(),
 });
+export const ParamsIdSchema = z.object({
+  id: z.string().regex(/^\d+$/, 'ID must be a number'),
+});
 
 export type CreateUserDto = z.infer<typeof CreateUserSchema>;
 export type UpdateUserDto = z.infer<typeof UpdateUserSchema>;
+export type ParamsIdDto = z.infer<typeof ParamsIdSchema>;
