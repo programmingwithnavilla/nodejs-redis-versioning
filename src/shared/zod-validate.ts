9430af6049f60a47bb78bc246143e6dff5bb8144
programmingwithnavilla <navidbarsalari@gmail.com>
2025-07-06 17:44:27 +0330

    add zod-validate

--- src/shared/zod-validate.ts
+++ src/shared/zod-validate.ts
@@ -2,15 +2,18 @@ import { Request, Response, NextFunction } from 'express';
 import { ZodSchema } from 'zod';
 
 export const zodValidate =
-  (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
+  (schema: ZodSchema) =>
+  (req: Request, res: Response, next: NextFunction): void => {
     const result = schema.safeParse(req.body);
+
     if (!result.success) {
-      return res.status(400).json({
+      res.status(400).json({
         message: 'Validation failed',
         errors: result.error.flatten().fieldErrors,
       });
+      return;
     }
 
-    req.body = result.data; // Validated & parsed
+    req.body = result.data;
     next();
   };
