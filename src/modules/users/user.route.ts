79e3ad1de2da8a75931f355e9083beb41cd3a809
programmingwithnavilla <navidbarsalari@gmail.com>
2025-07-06 17:44:34 +0330

    add user route

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
 import { container } from 'tsyringe';
 import { UserController } from './user.controller';
+import { zodValidate } from '../../shared/zod-validate';
+import { CreateUserSchema, UpdateUserSchema } from './user.schema';
 
 const router = Router();
 
 const userController = container.resolve(UserController);
 
 router.get('/', userController.getAll);
-router.post('/', userController.create);
-router.put('/:id', userController.update);
+router.post('/', zodValidate(CreateUserSchema), userController.create);
+router.put('/:id', zodValidate(UpdateUserSchema), userController.update);
 router.delete('/:id', userController.delete);
 
 export default router;
