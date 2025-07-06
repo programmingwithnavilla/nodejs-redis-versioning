1c806f4473c35f61daea063014403341e88ed9f9
programmingwithnavilla <navidbarsalari@gmail.com>
2025-07-06 18:04:04 +0330

    use zodValidate params in user.router

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import { container } from 'tsyringe';
 import { UserController } from './user.controller';
-import { zodValidate } from '../../shared/zod-validate';
-import { CreateUserSchema, UpdateUserSchema } from './user.schema';
+import { validateParams, zodValidate } from '../../shared/zod-validate';
+import {
+  CreateUserSchema,
+  ParamsIdSchema,
+  UpdateUserSchema,
+} from './user.schema';
 
 const router = Router();
 
@@ -10,7 +14,13 @@ const userController = container.resolve(UserController);
 
 router.get('/', userController.getAll);
 router.post('/', zodValidate(CreateUserSchema), userController.create);
-router.put('/:id', zodValidate(UpdateUserSchema), userController.update);
-router.delete('/:id', userController.delete);
+router.put(
+  '/:id',
+  validateParams(ParamsIdSchema),
+  zodValidate(UpdateUserSchema),
+  userController.update,
+);
+router.delete('/:id', validateParams(ParamsIdSchema), userController.delete);
 
 export default router;
+//
